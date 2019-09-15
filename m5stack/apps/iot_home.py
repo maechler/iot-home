3a62b61238bf827728d6b7018c46e97bba4c8181
Markus Mächler <markus.maechler@bithost.ch>
2019-09-15 10:15:10 +0200

    Adds some improvements and refactoring
    Signed-off-by: Markus Mächler <markus.maechler@bithost.ch>

--- m5stack/apps/iot_home.py
+++ m5stack/apps/iot_home.py
@@ -10,7 +10,15 @@ import utime
 import unit
 
 TFT_LED_PIN = const(32)
-SCREEN_TIMEOUT = 1000 * 60
+
+
+class MyTextBox(M5TextBox):
+    add_fake_space = True
+
+    def setText(self, text):
+        # use fake space to trick M5TextBox into rendering text even if unchanged
+        super().setText(text + (' ' if self.add_fake_space else ''))
+        self.add_fake_space = not self.add_fake_space
 
 
 class CoreApp:
@@ -63,6 +71,7 @@ class CoreApp:
         'core_id': 'inside',
         'broker_ip': '192.168.1.12',
         'send_frequency': 0.1,
+        'screen_timeout': 1000 * 60,
         'ui': {
             'default_color': 0xEEEEEE,
         },
@@ -71,7 +80,6 @@ class CoreApp:
     def __init__(self):
         self.isInitialzed = False
         self.screen_power = machine.Pin(TFT_LED_PIN, machine.Pin.OUT)
-        self.last_user_interaction = self.current_time()
 
     def init(self):
         if self.isInitialzed:
@@ -86,19 +94,13 @@ class CoreApp:
         self.isInitialzed = True
 
     def init_screen(self):
+        self.last_user_interaction = self.current_time()  # Startup
         self.set_screen_on(True)
+
         lcd.clear(lcd.BLACK)
 
         self.status_circle = M5Circle(20, 215, 10, 0xaaaaaa, 0xaaaaaa)
-        self.status_text = M5TextBox(40, 207, 'starting', lcd.FONT_DejaVu18, self.config['ui']['default_color'])
-
-    def set_screen_on(self, is_on):
-        self.screen_power.value(int(is_on))
-
-    def check_screen_timeout(self):
-        delta = self.current_time() - self.last_user_interaction
-        if delta > SCREEN_TIMEOUT:
-            self.set_screen_on(False)
+        self.status_text = MyTextBox(40, 207, 'starting', lcd.FONT_DejaVu18, self.config['ui']['default_color'])
 
     def init_sensors(self):
         self.active_sensors = {}
@@ -123,8 +125,8 @@ class CoreApp:
                 'config': sensor_config,
                 'get_value': lambda s=current_sensor, c=sensor_config: s.analogRead(c['pbhub_address']) if 'pbhub_address' in c else getattr(s, c['value_key']),
                 'sensor': current_sensor,
-                'label_text': M5TextBox(start_x, start_y + 30, sensor_config['label'] + ' [' + sensor_config['measurement_unit'] + ']', lcd.FONT_Default, CoreApp.config['ui']['default_color']),
-                'label_value': M5TextBox(start_x, start_y, '-', lcd.FONT_DejaVu24, CoreApp.config['ui']['default_color']),
+                'label_text': MyTextBox(start_x, start_y + 30, sensor_config['label'] + ' [' + sensor_config['measurement_unit'] + ']', lcd.FONT_Default, CoreApp.config['ui']['default_color']),
+                'label_value': MyTextBox(start_x, start_y, '-', lcd.FONT_DejaVu24, CoreApp.config['ui']['default_color']),
             }
 
             sensor_count = sensor_count + 1
@@ -144,12 +146,21 @@ class CoreApp:
     def init_time(self):
         settime()
 
+    def set_screen_on(self, is_on):
+        self.screen_power.value(int(is_on))
+
     @staticmethod
     def interrupt_handler(timer):
         CoreApp.interrupt_counter = 1 + CoreApp.interrupt_counter
 
     def current_time(self):
         return (utime.time() + 946684800) * 1000  # Convert utime to timestamp in milliseconds
+
+    def run_check_screen_timeout(self):
+        delta = self.current_time() - self.last_user_interaction
+
+        if delta > self.config['screen_timeout']:
+            self.set_screen_on(False)
 
     def run_decrement_interrupt_counter(self):
         state = machine.disable_irq()
@@ -174,30 +185,41 @@ class CoreApp:
             self.status_circle.setBorderColor(0xaaaaaa)
             self.status_text.setText('undefined status')
 
+    def run_show_config(self):
+        lcd.clear(lcd.BLACK)
+
+        start_x = 10
+        start_y = 10
+
+        core_id_label = MyTextBox(start_x, start_y + 30, 'Core ID', lcd.FONT_Default, CoreApp.config['ui']['default_color'])
+        core_id_value = MyTextBox(start_x, start_y, CoreApp.config['core_id'], lcd.FONT_DejaVu24, CoreApp.config['ui']['default_color'])
+        start_y = start_y + 65
+        mqtt_host_label = MyTextBox(start_x, start_y + 30, 'MQTT Host', lcd.FONT_Default, CoreApp.config['ui']['default_color'])
+        mqtt_host_value = MyTextBox(start_x, start_y, CoreApp.config['broker_ip'], lcd.FONT_DejaVu24, CoreApp.config['ui']['default_color'])
+        start_y = start_y + 65
+        send_frequency_label = MyTextBox(start_x, start_y + 30, 'Send Frequency [Hz]', lcd.FONT_Default, CoreApp.config['ui']['default_color'])
+        send_frequency_value = MyTextBox(start_x, start_y, str(CoreApp.config['send_frequency']), lcd.FONT_DejaVu24, CoreApp.config['ui']['default_color'])
+
     def run_buttons(self):
+        button_pressed = False
+
+        if btnA.wasPressed():
+            button_pressed = True
+
         if btnB.wasPressed():
-            lcd.clear(lcd.BLACK)
+            button_pressed = True
 
-            start_x = 10
-            start_y = 10
-
-            core_id_label = M5TextBox(start_x, start_y + 30, 'Core ID', lcd.FONT_Default, CoreApp.config['ui']['default_color'])
-            core_id_value = M5TextBox(start_x, start_y, CoreApp.config['core_id'], lcd.FONT_DejaVu24, CoreApp.config['ui']['default_color'])
-            start_y = start_y + 65
-            mqtt_host_label = M5TextBox(start_x, start_y + 30, 'MQTT Host', lcd.FONT_Default, CoreApp.config['ui']['default_color'])
-            mqtt_host_value = M5TextBox(start_x, start_y, CoreApp.config['broker_ip'], lcd.FONT_DejaVu24, CoreApp.config['ui']['default_color'])
-            start_y = start_y + 65
-            send_frequency_label = M5TextBox(start_x, start_y + 30, 'Send Frequency [Hz]', lcd.FONT_Default, CoreApp.config['ui']['default_color'])
-            send_frequency_value = M5TextBox(start_x, start_y, str(CoreApp.config['send_frequency']), lcd.FONT_DejaVu24, CoreApp.config['ui']['default_color'])
+            self.run_show_config()
 
             while not btnB.isReleased():
                 pass
 
             lcd.clear(lcd.BLACK)
 
-            return True
+        if btnC.wasPressed():
+            button_pressed = True
 
-        return False
+        return button_pressed
 
     def run(self):
         self.init()
@@ -205,23 +227,20 @@ class CoreApp:
 
         timer = machine.Timer(-1)
         timer.init(period=int(1000 / CoreApp.config['send_frequency']), mode=machine.Timer.PERIODIC, callback=CoreApp.interrupt_handler)
-        fake_space = True  # use fake space to trick M5TextBox to render text even if unchanged
 
         while True:
             buttons_pressed = self.run_buttons()
 
-            self.check_screen_timeout()
+            self.run_check_screen_timeout()
 
             if buttons_pressed:
                 self.last_user_interaction = self.current_time()
                 self.set_screen_on(True)
 
-                self.run_set_status('waiting' + (' ' if fake_space else ''))
+                self.run_set_status('waiting')
                 for sensor_name, sensor in self.active_sensors.items():
-                    sensor['label_text'].setText(sensor['config']['label'] + ' [' + sensor['config']['measurement_unit'] + ']' + (' ' if fake_space else ''))
-                    sensor['label_value'].setText('{:.2f}'.format(sensor['get_value']()) + (' ' if fake_space else ''))
-
-                fake_space = not fake_space
+                    sensor['label_text'].setText(sensor['config']['label'] + ' [' + sensor['config']['measurement_unit'] + ']')
+                    sensor['label_value'].setText('{:.2f}'.format(sensor['get_value']()))
 
             if CoreApp.interrupt_counter > 0:
                 self.run_set_status('sending')
@@ -230,14 +249,13 @@ class CoreApp:
                 for sensor_name, sensor in self.active_sensors.items():
                     sensor_json = {'timestamp': self.current_time(), 'value': sensor['get_value']()}
 
-                    sensor['label_text'].setText(sensor['config']['label'] + ' [' + sensor['config']['measurement_unit'] + ']' + (' ' if fake_space else ''))
-                    sensor['label_value'].setText('{:.2f}'.format(sensor['get_value']()) + (' ' if fake_space else ''))
+                    sensor['label_text'].setText(sensor['config']['label'] + ' [' + sensor['config']['measurement_unit'] + ']')
+                    sensor['label_value'].setText('{:.2f}'.format(sensor['get_value']()))
 
                     self.m5mqtt.publish('home/' + self.config['core_id'] + '/' + sensor_name, json.dumps(sensor_json))
 
                 wait_ms(500)
-                self.run_set_status('waiting' + (' ' if fake_space else ''))
-                fake_space = not fake_space
+                self.run_set_status('waiting')
 
             wait_ms(500)
 
